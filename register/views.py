19f368fdcf2773b0168caca2b45cbcacfb28b581
Rene <renev88@gmail.com>
2020-02-04 13:06:01 -0500

    Regiter con username = email

--- register/views.py
+++ register/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 from .forms import RegisterForm
 
 # Create your views here.
@@ -8,7 +11,12 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            obj = form.save(commit=False)
+            obj.username = form.cleaned_data.get('email')
+            email = form.cleaned_data.get('email')
+            messages.success(request, f'Usuario creado: {email}')
+            obj.save()
+
         return redirect('index:index')
     else:
         form = RegisterForm()
