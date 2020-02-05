78cfe92892f0d14268d6b4d673fdb7806061b6f1
Rene <renev88@gmail.com>
2020-02-05 08:56:22 -0500

    Search bar categories

---
+++ static/js/index/nav.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  //Select width nav categorias
+  console.log($("#width_tmp_select").width()-2)
+  $("#categoria_select").change(function(){
+    $("#width_tmp_option").html($("#categoria_select option:selected").text());
+    $(this).width($("#width_tmp_select").width()-20);
+
+    console.log($("#width_tmp_select").width())
+
+    $(".buscar").focus()
+  });
+
+})
